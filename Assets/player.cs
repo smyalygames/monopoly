00759d32f857b9edc3a6c47f7fe63df89f726817
smyalygames <smyalygames@gmail.com>
2020-03-05 10:48:13 +0000

    Started a player class

---
+++ Assets/player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+    public class Player
+    {
+
+        public int money; //Initializes the variable for money.
+        public int position = 0; //Positions vary from 0-39 (40 squares on the board) (Go is 0)
+
+        public Player()
+        {
+            
+        }
+
+        public Move(int length) //This moves the player a certain length (what they got from rolling the dice).
+        {
+            if (position + length < 40) //Checks if the player will not pass go.
+            {
+                position += length; //Adds the length that the player needs to move.
+            }
+            else //If they pass go.
+            {
+                position = length - (39 - position); //Makes it so that the position is 
+                money += 200; //Collect money as they pass go.
+
+            }
+        }
+
+        public goToJail() //If the player needs to go to jail.
+        {
+            position = 40; //Special position for jail.
+        }
+
+        public getOutOfJail(int length) //If the player is going out of jail.
+        {
+            position = 10; //Moves the player out of jail.
+            Move(length); //Then moves the player.
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/properties.cs
+++ Assets/properties.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class properties : MonoBehaviour
 {
 
     public class Property
