39c86bf8d27847ee0c8059e261da0255d07b2a08
Anthony Berg <smyalygames@gmail.com>
2020-07-15 17:53:57 +0100

    Created a main menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+	public void QuitGame()
+	{
+		Debug.Log("QUIT!");
+		Application.Quit();
+	}
+
+}
