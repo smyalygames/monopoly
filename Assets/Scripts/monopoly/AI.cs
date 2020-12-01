cdd06568a58f9aef9b84a3a0de239f0c770f6a29
Anthony <smyalygames@gmail.com>
2020-12-01 13:14:38 +0000

    Started AI programming

---
+++ Assets/Scripts/monopoly/AI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class AI
+{
+    /* TODO
+     * Write out a way to roll a dice.
+     * Buy properties
+     * Buy houses
+     * Trade
+     * Etc...
+     */
+}
